3cb8dcc846cfa24783e8fca67ba8646efb8cb486
Alexandre Carvalho <alexandre.carvalho@live.com>
2024-06-20 19:06:55 -0300

    Partial implementation of solution settings

---
+++ src/backend/Flexbyte.CadCliFor/Flexbyte.CadCliFor.API/Extensions/ServiceExtensions.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.OpenApi.Models;
+
+namespace Flexbyte.CadCliFor.API.Extensions
+{
+    public static class ServiceExtensions
+    {
+        public static void ConfigureSwagger(this IServiceCollection services) 
+        {
+            // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+            services.AddEndpointsApiExplorer();
+            services.AddSwaggerGen( options =>
+            {
+                options.SwaggerDoc("V1", new OpenApiInfo
+                {
+                    Version = "v1",
+                    Title = "Flexbyte CadCliFor API",
+                    Description = "Flexbyte CadCliFor API"
+                });
+            });
+        }
+
+        public static void ConfigureCors(this IServiceCollection services) =>
+            services.AddCors(options =>
+            {
+                options.AddPolicy("CorsPolicy", builder =>
+                    builder.AllowAnyOrigin()
+                    .AllowAnyMethod()
+                    .AllowAnyHeader()
+                    .WithExposedHeaders("X-Pagination"));
+            });        
+        
+    }
+}
--- src/backend/Flexbyte.CadCliFor/Flexbyte.CadCliFor.API/Program.cs
+++ src/backend/Flexbyte.CadCliFor/Flexbyte.CadCliFor.API/Program.cs
@@ -1,11 +1,13 @@
+using Flexbyte.CadCliFor.API.Extensions;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.ConfigureSwagger();
+builder.Services.ConfigureCors();
+
 
 var app = builder.Build();
 
